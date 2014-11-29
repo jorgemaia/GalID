f13b8d8e0cc7aeeacabe1d573c75520899fc470e
Ivan F. Martinez <ivanfm@ig.com.br>
2014-11-29 12:09:19 -0200

    suporte para LCD

--- IoT_2014_NFC/IoT_2014_NFC.ino
+++ IoT_2014_NFC/IoT_2014_NFC.ino
@@ -6,6 +6,11 @@
 #include <SPI.h>
 #include <stdio.h>
 #include <MFRC522.h>
+
+// https://github.com/Seeed-Studio/Grove_LCD_RGB_Backlight.git
+#include <Wire.h>
+#include <rgb_lcd.h>
+
 #include <SD.h>
 
 #define RST_PIN		9		// 
@@ -17,6 +22,8 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance
 #define FALSE 0
 
 #define MAX_ID_BUFFER_LEN 30
+
+rgb_lcd lcd;
 
 
 // Caso o firmware esteja montando em outro diretorio nao funciona
@@ -46,19 +53,24 @@ void tratarCartao() {
          strcpy(fname, PATH_LIBERADOS);
          strcat(fname, idBuffer);
          Serial.println(fname);
-  
+    
+         lcd.print(idBuffer);
+         
          if (SD.exists(fname)) {
              //TODO usuario tem acesso
                Serial.println("Acesso Liberado");
+               lcd.setRGB(0x00,0xff,0x00);
          } else {
              strcpy(fname, PATH_BLOQUEADOS);
              strcat(fname, idBuffer);
              Serial.println(fname);
              if (SD.exists(fname)) {
+               lcd.setRGB(0xff,0x00,0x00);
                Serial.println("Acesso Bloqueado");
                 // TODO usuario tem acesso bloqueado/proibido
              } else {
                Serial.println("Nao tem acesso");
+               lcd.setRGB(0xff,0xff,0x00);
              // TODO usuario nao tem acesso
              }
          }       
@@ -66,7 +78,10 @@ void tratarCartao() {
 }
 
 void setup() {
+
         delay(5000);
+        lcd.begin(16, 2);
+        
 	Serial.begin(115200);		// Initialize serial communications with the PC
 	SPI.begin();			// Init SPI bus
 	mfrc522.PCD_Init();		// Init MFRC522
